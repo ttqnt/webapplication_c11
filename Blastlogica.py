c5106f7091a1feff8f6d01fc13356f6964c484ab
krimvo <K.Geertse@student.han.nl>
2022-08-20 11:15:49 +0200

    added project

---
+++ Blastlogica.py
@@ -0,0 +1,98 @@
+from app import *
+from Bio.Blast import NCBIWWW, NCBIXML
+
+# class voor alignmentpresentatiemethoden, om ze overzichtelijker op de webpagina te kunnen presenteren.
+class AlignmentPresentation:
+    def __init__(self, query, sbjct):
+        self.query = query
+        self.sbjct = sbjct
+        self.match = self.setMatch()
+        self.full_seq = self.full_seq()
+
+    def setMatch(self):
+        m = ""
+        i= 0
+        while len(self.query)-1 >= i:
+            if self.query[i] == self.sbjct[i]:
+                m += "|"
+                i += 1
+            else:
+                m += " "
+                i += 1
+        return m
+
+    def full_seq(self):
+        alignment = ""
+        i = 0
+        while len(self.query)-76 >= i:
+            alignment += self.query[i:i+75] + "\n" + self.match[i:i+75] + "\n" + self.sbjct[i:i+75] + "\n\n"
+            i+=75
+        alignment += self.query[i:len(self.query)-1] + "\n" + self.match[i:len(self.query)-1] + "\n" + self.sbjct[i:len(self.query)-1]
+        return alignment
+
+#blast een inputsequentie met qblast, en returns een met NCBIXML parsed bestand van de alignments.
+def blast_seq(input_seq, seq_type):
+    if seq_type == "nuc":
+        result_handle = NCBIXML.read(NCBIWWW.qblast("blastn", "nt", input_seq))
+    else:
+        result_handle = NCBIXML.read(NCBIWWW.qblast("blastp", "nr", input_seq))
+    return result_handle
+
+#returns nested dictionary van de ingevoerde sequenties op volgorde van invoeren,
+#de accessienummers van de alignments met bijbehorende annotatie.
+#(handled dus meerdere sequenties uit een .fasta bestand.)
+def fastaBlastRecords(blast_record):
+    blast_records = {}
+    i = 1
+    for record in blast_record:
+        alignments = {}
+        for alignment in record.alignments:
+            for hsp in alignment.hsps:
+                if ">" in alignment.hit_def:
+                    title = alignment.hit_def[:alignment.hit_def.index(">")]
+                else:
+                    title = alignment.hit_def
+                alignments[alignment.accession] = {
+                    "accessionnr" : alignment.accession,
+                    "hit_id" : alignment.hit_id,
+                    "hit_def" : title,
+                    "length" : alignment.length,
+                    "expect" : hsp.expect,
+                    "score" : hsp.score,
+                    "identities" : hsp.identities,
+                    "positives": hsp.positives,
+                    "gaps" : hsp.gaps,
+                    "query" : hsp.query,
+                    "sbjct" : hsp.sbjct
+                }
+                alignObject = AlignmentPresentation(hsp.query, hsp.sbjct)
+                alignments[alignment.accession].update({"match" : alignObject.match, "full_seq" : alignObject.full_seq})
+        blast_records[i] = alignments
+        i+=1
+    return blast_records
+
+#returns dictionary van alignment.accessienummers met annotatiegegevens van de alignment.
+def seqBlastRecords(blast_record):
+    alignments = {}
+    for alignment in blast_record.alignments:
+        for hsp in alignment.hsps:
+            if ">" in alignment.hit_def:
+                title = alignment.hit_def[:alignment.hit_def.index(">")]
+            else:
+                title = alignment.hit_def
+            alignments[alignment.accession] = {
+                "accessionnr" : alignment.accession,
+                "hit_id" : alignment.hit_id,
+                "hit_def" : title,
+                "length" : alignment.length,
+                "expect" : hsp.expect,
+                "score" : hsp.score,
+                "identities" : hsp.identities,
+                "positives": hsp.positives,
+                "gaps" : hsp.gaps,
+                "query" : hsp.query,
+                "sbjct" : hsp.sbjct
+                }
+            alignObject = AlignmentPresentation(hsp.query, hsp.sbjct)
+            alignments[alignment.accession].update({"match" : alignObject.match, "full_seq" : alignObject.full_seq})
+    return alignments
---
+++ app.py
@@ -0,0 +1,82 @@
+from flask import Flask, render_template, request, redirect, jsonify
+from flask_wtf import FlaskForm
+from wtforms import StringField, FileField, SubmitField, RadioField, TextAreaField
+from wtforms.validators import DataRequired
+from Blastlogica import *
+import redis
+#from Bio import Entrez, Medline
+
+app = Flask(__name__)
+cache = redis.Redis(host='redis', port=6379)
+app.config['SECRET_KEY'] = 'mykey'
+app.config["UPLOAD_FOLDER"] = "tmp/"
+
+#genereert user input form objecten voor de HTML pagina.
+class InputForm(FlaskForm):
+    sequence = TextAreaField()
+    file = FileField('File')
+    seqType = RadioField('Sequentietype:',
+                            choices=[('nuc', 'DNA/RNA'), ('prot', 'Proteine')],validators=[DataRequired()])
+    ziekte = StringField("Ziekte:")
+    submit = SubmitField('Submit')
+
+#REST API methode; accepteert json/dictionary van 1 of meerdere sequenties met sequentietype ("nuc", "prot")
+#en evt een ziekte als value, retourneert een jsonfile van de annotatie.
+#redirects naar de webpagina /Blasttool wanneer er geen jsonfile aangeleverd wordt. 
+@app.route('/', methods = ['GET', 'POST'])
+def main():
+    if request.method == 'GET':
+        return redirect('/BlastTool')
+    if request.method == 'POST':
+        i = 1
+        blast_records = {}
+        response = request.get_json()
+        for sequence in response:
+            blast_records[i] = seqBlastRecords(blast_seq(sequence, response[sequence]["seqType"]))
+            i += 1
+        return jsonify(blast_records)
+
+#methode voor renderen van de webpaginas en het accepteren en verwerken en retourneren van user input.
+@app.route('/BlastTool', methods = ['GET', 'POST'])
+def index():
+    error = None
+    form = InputForm(request.form)
+
+    if form.validate_on_submit():
+        input_seq = form.sequence.data
+        seqType = form.seqType.data
+        ziekte = form.ziekte.data
+        file = request.files['file']
+
+        if not file and input_seq == "":
+            error = "Voer een sequentie of fastfile in"
+            return render_template("index.html", form=form, error=error)
+        elif input_seq != "":
+            i = 1
+            blast_records = {}
+            sequences = input_seq.split("\n")
+            if "" in sequences:
+                sequences.remove("")
+            for seq in sequences:
+                blast_records[i] = seqBlastRecords(blast_seq(seq, seqType))
+                i += 1
+            return render_template('content.html', blast_records=blast_records)
+        else:
+            file.save(app.config['UPLOAD_FOLDER']+ "fasta_upload")
+            blast_records = fastaBlastRecords(blast_fasta(seqType))
+            return render_template('content.html', blast_records=blast_records)
+
+    return render_template('index.html', form=form)
+
+#methode die het aangeleverde .fasta bestand inleest, met qBlast runt en parsed.
+#retourneert een NCBIXML parsed bestand van de alignments.
+def blast_fasta(seq_type):
+    fasta_string = open(app.config['UPLOAD_FOLDER'] + "fasta_upload","r").read()
+    if seq_type == "nuc":
+        result_handle = NCBIXML.parse(NCBIWWW.qblast("blastn", "nt", fasta_string))
+    else:
+        result_handle = NCBIXML.parse(NCBIWWW.qblast("blastp", "nr", fasta_string))
+    return result_handle
+
+if __name__ == '__main__':
+    app.run(debug = True)
